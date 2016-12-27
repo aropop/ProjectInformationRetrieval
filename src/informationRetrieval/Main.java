0a6b256f32af9a71b55953d222a6e2b0db9ada84
Aropop <aropop@gmail.com>
2016-12-27 14:58:00 +0100

    Made search work, made aspect 1 work, highlighted aspects

--- src/informationRetrieval/Main.java
+++ src/informationRetrieval/Main.java
@@ -20,10 +20,17 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -32,7 +39,7 @@ public class Main {
 		  
 
 	  /** Index all text files under a directory. */
-	  public void index(String indexPath, Path docDir) {
+	  public static void index(String indexPath, Path docDir) {
 	    Date start = new Date();
 	    try {
 	      System.out.println("Indexing to directory '" + indexPath + "'...");
@@ -58,26 +65,26 @@ public class Main {
 	    }
 	  }
 	  
-	  private void indexDocs(final IndexWriter writer, Path path) throws IOException {
-	    if (Files.isDirectory(path)) {
-	      Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-	        @Override
-	        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-	          try {
-	            indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
-	          } catch (IOException ignore) {
-	            // don't index files that can't be read.
-	          }
-	          return FileVisitResult.CONTINUE;
-	        }
-	      });
-	    } else {
-	      indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
-	    }
+	  private static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+	      if (Files.isDirectory(path)) {
+		      Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+		          @Override
+		          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		              try {
+		                  indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+		              } catch (IOException ignore) {
+		                  System.out.println("IO error on adding file");
+		              }
+		              return FileVisitResult.CONTINUE;
+		          }
+		      });
+		  } else {
+		      indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+		  }
 	  }
 
 	  /** Indexes a single document */
-	  static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+	  private static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
 	    try (InputStream stream = Files.newInputStream(file)) {
 	      // make a new, empty document
 	      Document doc = new Document();
@@ -93,9 +100,54 @@ public class Main {
           writer.addDocument(doc);
 	    }
 	  }
+	  
+	  /**
+	   * 
+	   * Query syntax see http://lucene.apache.org/core/6_3_0/queryparser/index.html
+	   * 
+	   * @param query
+	   * @param indexPath
+	   */
+	  private static void search(String queryStr, String indexPath) {
+		  IndexReader reader;
+		  try {
+			  reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+			  IndexSearcher searcher = new IndexSearcher(reader);
+			  Analyzer analyzer = new StandardAnalyzer();
+			  QueryParser parser = new QueryParser("contents", analyzer);
+			  Query query = parser.parse(queryStr);
+			  TopDocs res = searcher.search(query, 100);
+			  ScoreDoc[] hits = res.scoreDocs;
+		      
+		      for (int i = 0; i < hits.length; i++) {
+				Document doc = searcher.doc(hits[i].doc);
+				float score = hits[i].score;
+				String path = doc.get("path");
+				if (path != null) {
+				  System.out.println((i+1) + ". " + score + " -> " + path);
+				  String title = doc.get("title");
+				  if (title != null) {
+				    System.out.println("   Title: " + doc.get("title"));
+				  }
+				} else {
+				  System.out.println((i+1) + ". " + "No path for this document");
+				}
+		      }
+		  } catch (IOException e) {
+			  System.out.println("IO error: " + e.getMessage());
+			  e.printStackTrace();
+		  } catch (ParseException e) {
+			  System.out.println("Parser error: " + e.getMessage());
+			  e.printStackTrace();
+		}
+	  }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(args[0].equals("index")) {
+			index(args[2], Paths.get(args[1]));
+		} else if(args[0].equals("search")) {
+			search(args[1], args[2]);
+		}
 
 	}
 
